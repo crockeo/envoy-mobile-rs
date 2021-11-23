6fadedfdcb179e6856bbfa1a2203829bd0fb3ec3
Cerek Hillen <cerekh@gmail.com>
2021-11-23 14:51:26 -0800

    engine lifecycle in async

--- src/event.rs
+++ src/event.rs
@@ -1,33 +1,83 @@
-use std::sync::{Condvar, Mutex};
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::{Arc, Condvar, Mutex};
+use std::task::{Context, Poll, Waker};
+
+// TODO: introducing the waker here makes things more complicated
+// and i'm not sure that everything is water tight
+// e.g. what happens if two different calls try to wait on the same event?
 
 pub struct Event {
     occurred: Mutex<bool>,
     condvar: Condvar,
+    waker: Mutex<Option<Waker>>,
 }
 
 impl Event {
     pub fn new() -> Self {
-	Self {
-	    occurred: Mutex::new(false),
-	    condvar: Condvar::new(),
-	}
+        Self {
+            occurred: Mutex::new(false),
+            condvar: Condvar::new(),
+            waker: Mutex::new(None),
+        }
     }
 
     pub fn set(&self) {
-	let mut guard = self.occurred.lock().unwrap();
-	if !*guard {
-	    (*guard) = true;
-	    self.condvar.notify_all();
-	}
+        let mut guard = self.occurred.lock().unwrap();
+        if !*guard {
+            (*guard) = true;
+            self.condvar.notify_all();
+
+            let mut waker_guard = self.waker.lock().unwrap();
+            if let Some(waker) = waker_guard.take() {
+                waker.wake();
+            }
+        }
     }
 
     pub fn is_set(&self) -> bool {
-	let guard = self.occurred.lock().unwrap();
-	*guard
+        let guard = self.occurred.lock().unwrap();
+        *guard
     }
 
     pub fn wait(&self) {
-	let guard = self.occurred.lock().unwrap();
-	let _ = self.condvar.wait_while(guard, |occurred| !(*occurred));
+        let guard = self.occurred.lock().unwrap();
+        let _ = self.condvar.wait_while(guard, |occurred| !(*occurred));
+    }
+
+    pub fn set_waker(&self, waker: Waker) {
+        let mut guard = self.waker.lock().unwrap();
+        (*guard) = Some(waker);
+    }
+}
+
+pub struct EventFuture<T> {
+    value: Option<T>,
+    occurred: Arc<Event>,
+}
+
+impl<T> EventFuture<T> {
+    pub fn new(value: T, occurred: Arc<Event>) -> Self {
+        EventFuture {
+            value: Some(value),
+            occurred,
+        }
+    }
+}
+
+impl<T: Unpin> Future for EventFuture<T> {
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.occurred.is_set() {
+            Poll::Ready(
+                self.value
+                    .take()
+                    .expect("FutureValue must not be polled after marking itself as Ready"),
+            )
+        } else {
+	    self.occurred.set_waker(ctx.waker().clone());
+            Poll::Pending
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,79 @@
 mod bridge;
+mod event;
 mod sys;
 
-pub use bridge::*;
+use std::sync::Arc;
+
+pub use bridge::LogLevel;
+
+pub struct EngineBuilder {
+    builder: bridge::EngineBuilder<EngineContext>,
+    context: EngineContext,
+}
+
+impl EngineBuilder {
+    pub fn new() -> Self {
+        let context = EngineContext::new();
+        Self {
+            builder: bridge::EngineBuilder::new(context.clone())
+                .with_on_engine_running(|ctx| ctx.engine_running.set())
+                .with_on_exit(|ctx| ctx.engine_terminated.set()),
+            context,
+        }
+    }
+
+    pub fn build(self, log_level: LogLevel) -> event::EventFuture<Engine> {
+	event::EventFuture::new(
+	    Engine {
+		engine: self.builder.build(log_level),
+		context: self.context.clone(),
+	    },
+	    self.context.engine_running,
+	)
+    }
+}
+
+#[derive(Clone)]
+struct EngineContext {
+    engine_running: Arc<event::Event>,
+    engine_terminated: Arc<event::Event>,
+}
+
+impl EngineContext {
+    fn new() -> Self {
+        Self {
+            engine_running: Arc::new(event::Event::new()),
+            engine_terminated: Arc::new(event::Event::new()),
+        }
+    }
+}
+
+pub struct Engine {
+    engine: bridge::Engine,
+    context: EngineContext,
+}
+
+impl Engine {
+    pub fn terminate(self) -> event::EventFuture<()> {
+	self.engine.terminate();
+	event::EventFuture::new((), self.context.engine_terminated)
+    }
+}
+
+struct StreamContext {
+}
+
+pub struct Stream {}
 
 #[cfg(test)]
 mod tests {
+    use tokio::test;
+
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    async fn engine_lifecycle() {
+        let engine = EngineBuilder::new().build(LogLevel::Debug).await;
+        engine.terminate().await;
     }
 }
