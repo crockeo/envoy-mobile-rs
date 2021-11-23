bcb7230bb683f15f09c00969fa13d6423eebbad6
Cerek Hillen <cerekh@gmail.com>
2021-11-23 15:01:26 -0800

    builder functions on async EngineBuilder

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@ mod bridge;
 mod event;
 mod sys;
 
+use std::net::IpAddr;
 use std::sync::Arc;
 
-pub use bridge::LogLevel;
+pub use bridge::{EventTrackerTrack, LogLevel, LoggerLog};
 
 pub struct EngineBuilder {
     builder: bridge::EngineBuilder<EngineContext>,
@@ -22,14 +23,128 @@ impl EngineBuilder {
         }
     }
 
+    pub fn with_log(mut self, log: LoggerLog) -> Self {
+        self.builder = self.builder.with_log(log);
+        self
+    }
+
+    pub fn with_track(mut self, track: EventTrackerTrack) -> Self {
+        self.builder = self.builder.with_track(track);
+        self
+    }
+
+    pub fn with_connect_timeout_seconds(mut self, connect_timeout_seconds: usize) -> Self {
+        self.builder = self
+            .builder
+            .with_connect_timeout_seconds(connect_timeout_seconds);
+        self
+    }
+
+    pub fn with_dns_refresh_rate_seconds(mut self, dns_refresh_rate_seconds: usize) -> Self {
+        self.builder = self
+            .builder
+            .with_dns_refresh_rate_seconds(dns_refresh_rate_seconds);
+        self
+    }
+
+    pub fn with_dns_fail_interval(
+        mut self,
+        dns_fail_base_interval_seconds: usize,
+        dns_fail_max_interval_seconds: usize,
+    ) -> Self {
+        self.builder = self.builder.with_dns_fail_interval(
+            dns_fail_base_interval_seconds,
+            dns_fail_max_interval_seconds,
+        );
+        self
+    }
+
+    pub fn with_dns_query_timeout_seconds(mut self, dns_query_timeout_seconds: usize) -> Self {
+        self.builder = self
+            .builder
+            .with_dns_query_timeout_seconds(dns_query_timeout_seconds);
+        self
+    }
+
+    pub fn with_enable_interface_binding(mut self, enable_interface_binding: bool) -> Self {
+        self.builder = self
+            .builder
+            .with_enable_interface_binding(enable_interface_binding);
+        self
+    }
+
+    pub fn with_h2_connection_keepalive_idle_interval_seconds(
+        mut self,
+        h2_connection_keepalive_idle_interval_seconds: usize,
+    ) -> Self {
+        self.builder = self
+            .builder
+            .with_h2_connection_keepalive_idle_interval_seconds(
+                h2_connection_keepalive_idle_interval_seconds,
+            );
+        self
+    }
+
+    pub fn with_h2_connection_keepalive_timeout(
+        mut self,
+        h2_connection_keepalive_timeout: usize,
+    ) -> Self {
+        self.builder = self
+            .builder
+            .with_h2_connection_keepalive_timeout(h2_connection_keepalive_timeout);
+        self
+    }
+
+    pub fn with_stats_domain(mut self, stats_domain: IpAddr) -> Self {
+        self.builder = self.builder.with_stats_domain(stats_domain);
+        self
+    }
+
+    pub fn with_stats_flush_interval_seconds(
+        mut self,
+        stats_flush_interval_seconds: usize,
+    ) -> Self {
+        self.builder = self
+            .builder
+            .with_stats_flush_interval_seconds(stats_flush_interval_seconds);
+        self
+    }
+
+    pub fn with_statsd_host(mut self, statsd_host: IpAddr) -> Self {
+        self.builder = self.builder.with_statsd_host(statsd_host);
+        self
+    }
+
+    pub fn with_statsd_port(mut self, statsd_port: u16) -> Self {
+        self.builder = self.builder.with_statsd_port(statsd_port);
+        self
+    }
+
+    pub fn with_stream_idle_timeout_seconds(mut self, stream_idle_timeout_seconds: usize) -> Self {
+        self.builder = self
+            .builder
+            .with_stream_idle_timeout_seconds(stream_idle_timeout_seconds);
+        self
+    }
+
+    pub fn with_per_try_idle_timeout_seconds(
+        mut self,
+        per_try_idle_timeout_seconds: usize,
+    ) -> Self {
+        self.builder = self
+            .builder
+            .with_per_try_idle_timeout_seconds(per_try_idle_timeout_seconds);
+        self
+    }
+
     pub fn build(self, log_level: LogLevel) -> event::EventFuture<Engine> {
-	event::EventFuture::new(
-	    Engine {
-		engine: self.builder.build(log_level),
-		context: self.context.clone(),
-	    },
-	    self.context.engine_running,
-	)
+        event::EventFuture::new(
+            Engine {
+                engine: self.builder.build(log_level),
+                context: self.context.clone(),
+            },
+            self.context.engine_running,
+        )
     }
 }
 
@@ -55,13 +170,12 @@ pub struct Engine {
 
 impl Engine {
     pub fn terminate(self) -> event::EventFuture<()> {
-	self.engine.terminate();
-	event::EventFuture::new((), self.context.engine_terminated)
+        self.engine.terminate();
+        event::EventFuture::new((), self.context.engine_terminated)
     }
 }
 
-struct StreamContext {
-}
+struct StreamContext {}
 
 pub struct Stream {}
 
@@ -73,7 +187,12 @@ mod tests {
 
     #[test]
     async fn engine_lifecycle() {
-        let engine = EngineBuilder::new().build(LogLevel::Debug).await;
+        let engine = EngineBuilder::new()
+            .with_log(Box::new(|data| {
+                print!("{}", String::try_from(data).unwrap());
+            }))
+            .build(LogLevel::Debug)
+            .await;
         engine.terminate().await;
     }
 }
