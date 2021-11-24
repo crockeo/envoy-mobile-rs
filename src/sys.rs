2caee914025577f0d70ed806fc56adc2a2908b6f
Cerek Hillen <cerekh@gmail.com>
2021-11-23 19:01:47 -0800

    allow more things from sys.rs to reduce spurious warnings

--- src/sys.rs
+++ src/sys.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
+#![allow(deref_nullptr)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![allow(unaligned_references)]
 
 #[link(name = "envoy_mobile")]
 extern "C" {
