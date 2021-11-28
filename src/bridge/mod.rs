986fcab66cd1294ff9b8f57df3fe56557abe8ded
Cerek Hillen <cerekh@gmail.com>
2021-11-28 01:39:45 -0800

    make data a nice little home

--- src/bridge/mod.rs
+++ src/bridge/mod.rs
@@ -1,15 +1,17 @@
+mod data;
+
 use std::alloc;
 use std::collections::{BTreeMap, HashMap};
 use std::ffi;
 use std::net::{IpAddr, Ipv4Addr};
-use std::ptr;
-use std::str::Utf8Error;
 use std::string::FromUtf8Error;
 use std::sync::Arc;
 
 use crate::channel::{Channel, ReadOnlyChannel};
 use crate::event::{Event, EventFuture};
 use crate::sys;
+
+pub use data::*;
 
 // some things i don't like and want to work on
 //
@@ -838,80 +840,6 @@ impl Network {
     }
 }
 
-#[derive(Eq, Clone, Debug, PartialEq)]
-pub struct Data(Vec<u8>);
-
-impl From<&str> for Data {
-    fn from(string: &str) -> Self {
-        Data::from(string.to_owned())
-    }
-}
-
-impl From<String> for Data {
-    fn from(string: String) -> Self {
-        Self(Vec::from_iter(string.into_bytes()))
-    }
-}
-
-impl From<Vec<u8>> for Data {
-    fn from(data: Vec<u8>) -> Self {
-        Data(data)
-    }
-}
-
-impl<'a> TryFrom<&'a Data> for &'a str {
-    type Error = Utf8Error;
-
-    fn try_from(data: &'a Data) -> Result<&'a str, Self::Error> {
-        std::str::from_utf8(&data.0)
-    }
-}
-
-impl TryFrom<Data> for String {
-    type Error = FromUtf8Error;
-
-    fn try_from(data: Data) -> Result<String, Self::Error> {
-        String::from_utf8(data.into())
-    }
-}
-
-impl From<Data> for Vec<u8> {
-    fn from(data: Data) -> Vec<u8> {
-        data.0
-    }
-}
-
-impl Data {
-    unsafe fn from_envoy_data_no_release(envoy_data: &sys::envoy_data) -> Self {
-	let length = envoy_data.length.try_into().unwrap();
-	let mut bytes = vec![0; length];
-	ptr::copy(envoy_data.bytes, bytes.as_mut_ptr(), length);
-	Self(bytes)
-    }
-
-    unsafe fn from_envoy_data(envoy_data: sys::envoy_data) -> Self {
-        let data = Self::from_envoy_data_no_release(&envoy_data);
-        sys::release_envoy_data(envoy_data);
-        data
-    }
-
-    fn into_envoy_data(self) -> sys::envoy_data {
-        let vec = Box::new(self.0);
-        let data_ptr = vec.as_ptr();
-        let length = vec.len();
-
-        sys::envoy_data {
-            length: length.try_into().unwrap(),
-            bytes: data_ptr,
-            release: Some(Data::release_data),
-            context: Box::into_raw(vec) as *mut ffi::c_void,
-        }
-    }
-
-    unsafe extern "C" fn release_data(context: *mut ffi::c_void) {
-        let _ = Box::from_raw(context as *mut Vec<u8>);
-    }
-}
 
 #[derive(Debug)]
 pub struct Map(Vec<(Data, Data)>);
