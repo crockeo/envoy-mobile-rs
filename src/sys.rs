326bba807b61e38a6135a440fea5a217259186d3
Cerek Hillen <cerekh@gmail.com>
2021-11-28 15:32:33 -0800

    better config template loading

--- src/sys.rs
+++ src/sys.rs
@@ -5,9 +5,4 @@
 #![allow(non_upper_case_globals)]
 #![allow(unaligned_references)]
 
-#[link(name = "envoy_mobile")]
-extern "C" {
-    pub static config_template: *const ::std::os::raw::c_char;
-}
-
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
--- update_wrapper.sh
+++ update_wrapper.sh
@@ -4,5 +4,6 @@ echo "\
 // Copied from envoy-mobile (https://github.com/envoyproxy/envoy-mobile).
 // Which is licensed under Apache 2.0. Code copyright
 // the Envoy Project Authors." > wrapper.h
+cat envoy-mobile/library/common/config/templates.h >> wrapper.h
 cat envoy-mobile/library/common/types/c_types.h >> wrapper.h
 cat envoy-mobile/library/common/main_interface.h | grep -v "#include \"library.\\+\"" >> wrapper.h
